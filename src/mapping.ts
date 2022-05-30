df9830b5c131af7e4a8d23ab5024031bb1d1bbd0
Shantanu Gupta <shantanu@biconomy.io>
2022-05-30 11:30:01 +0530

    first commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -14,93 +14,69 @@ import {
   Transfer,
   UserIndexUpdated
 } from "../generated/StakingContract/StakingContract"
-import { ExampleEntity } from "../generated/schema"
-
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.COOLDOWN_SECONDS(...)
-  // - contract.DELEGATE_BY_TYPE_TYPEHASH(...)
-  // - contract.DELEGATE_TYPEHASH(...)
-  // - contract.DISTRIBUTION_END(...)
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.EIP712_REVISION(...)
-  // - contract.EMISSION_MANAGER(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.REVISION(...)
-  // - contract.REWARDS_VAULT(...)
-  // - contract.REWARD_TOKEN(...)
-  // - contract.STAKED_TOKEN(...)
-  // - contract.UNSTAKE_WINDOW(...)
-  // - contract._aaveGovernance(...)
-  // - contract._nonces(...)
-  // - contract._votingSnapshots(...)
-  // - contract._votingSnapshotsCounts(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.assets(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.getDelegateeByType(...)
-  // - contract.getNextCooldownTimestamp(...)
-  // - contract.getPowerAtBlock(...)
-  // - contract.getPowerCurrent(...)
-  // - contract.getTotalRewardsBalance(...)
-  // - contract.getUserAssetData(...)
-  // - contract.increaseAllowance(...)
-  // - contract.isTrustedForwarder(...)
-  // - contract.name(...)
-  // - contract.stakerRewardsToClaim(...)
-  // - contract.stakersCooldowns(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.totalSupplyAt(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-}
+import {Redeem as RedeemEntity, Staked as StakedEntity, RewardsAccrued as RewardsAccruedEntity, RewardsClaimed as RewardsClaimedEntity, UserStakedTotal, UniqueWallet, UniqueWalletCount, RollingStakedForLast24Hour, UserRedeemedTotal, RollingRedeemedForLast24Hour, CoolDownUser as CoolDownEntity, CoolDown ,CoolDownCount, RollingCoolDownForLast24Hour}  from "../generated/schema"
 
 export function handleAssetConfigUpdated(event: AssetConfigUpdated): void {}
 
 export function handleAssetIndexUpdated(event: AssetIndexUpdated): void {}
 
-export function handleCooldown(event: Cooldown): void {}
+export function handleCooldown(event: Cooldown): void {
+  const CoolDownUser = new CoolDownEntity(event.transaction.hash.toHex());
+  CoolDownUser.userAddress = event.params.user;
+  CoolDownUser.timestamp = event.block.timestamp;
+  CoolDownUser.save();
+
+  let coolDown = CoolDown.load(event.transaction.hash.toHex());
+  if (coolDown == null) {
+    let coolDownCount = CoolDownCount.load("0");
+
+    if (coolDownCount == null) {
+      coolDownCount = new CoolDownCount("0");
+      coolDownCount.count = BigInt.fromI32(0);
+    }
+
+    coolDown = new CoolDown(event.transaction.hash.toHex());
+    coolDown.count = BigInt.fromI32(0);
+
+    coolDownCount.count = coolDownCount.count.plus(BigInt.fromI32(1));
+
+    coolDownCount.save();
+
+    let slidingCoolDownWindow = RollingCoolDownForLast24Hour.load("0");
+
+    if (!slidingCoolDownWindow) {
+      slidingCoolDownWindow = new RollingCoolDownForLast24Hour("0");
+      slidingCoolDownWindow.count = BigInt.fromI32(0);
+      slidingCoolDownWindow.cooldowns = new Array<string>();
+    }
+  
+    let oldCoolDownLogs = slidingCoolDownWindow.cooldowns;
+    let newCoolDownLogs = new Array<string>();
+    newCoolDownLogs.push(CoolDownUser.id);
+  
+    // add the current feeDetailLogEntry to the cumulative values
+    slidingCoolDownWindow.count += BigInt.fromI32(1);
+  
+    // sliding window calculation
+    for (let i = 0; i < oldCoolDownLogs.length; i++) {
+      // for every feeDetailLogEntry in the rolling window, check if they are old enough to remove
+      // if so, then remove and also decrease their values from cumulative rolling window values
+      let oldCoolDown = CoolDownEntity.load(oldCoolDownLogs[i]);
+      if (!oldCoolDown) continue;
+      if (CoolDownUser.timestamp.minus(oldCoolDown.timestamp) > BigInt.fromI32(86400)) {
+        slidingCoolDownWindow.count -= BigInt.fromI32(1);
+      } else {
+        newCoolDownLogs.push(oldCoolDown.id);
+      }
+    }
+    slidingCoolDownWindow.cooldowns = newCoolDownLogs;
+    slidingCoolDownWindow.save();
+  }
+
+  coolDown.count = coolDown.count.plus(BigInt.fromI32(1));
+  coolDown.save();
+
+}
 
 export function handleDelegateChanged(event: DelegateChanged): void {}
 
@@ -108,13 +84,151 @@ export function handleDelegatedPowerChanged(
   event: DelegatedPowerChanged
 ): void {}
 
-export function handleRedeem(event: Redeem): void {}
+export function handleRedeem(event: Redeem): void {
+  const Redeem = new RedeemEntity(event.transaction.hash.toHex());
+  Redeem.userAddress = event.params.to;
+  Redeem.amount = event.params.amount.div(BigInt.fromI64(1000000000000000000));
+  Redeem.timestamp = event.block.timestamp;
+  Redeem.save();
 
-export function handleRewardsAccrued(event: RewardsAccrued): void {}
+  let userRedeemedTotal = UserRedeemedTotal.load("0");
+  if (!userRedeemedTotal){
+    userRedeemedTotal = new UserRedeemedTotal("0");
+    userRedeemedTotal.totalRedeemed = BigInt.fromI32(0);
+    userRedeemedTotal.timesRedeemed = BigInt.fromI32(0);
+  }
+  userRedeemedTotal.totalRedeemed += Redeem.amount;
+  userRedeemedTotal.timesRedeemed += BigInt.fromI32(1);
+  userRedeemedTotal.save();
 
-export function handleRewardsClaimed(event: RewardsClaimed): void {}
+  let slidingRedeemWindow = RollingRedeemedForLast24Hour.load("0");
 
-export function handleStaked(event: Staked): void {}
+  if (!slidingRedeemWindow) {
+    slidingRedeemWindow = new RollingRedeemedForLast24Hour("0");
+    slidingRedeemWindow.cumulativeAmount = BigInt.fromI32(0);
+    slidingRedeemWindow.count = BigInt.fromI32(0);
+    slidingRedeemWindow.redeems = new Array<string>();
+  }
+
+  // add the current feeDetailLogEntry to the sliding window
+  // deposit.rollingWindow = slidingWindow.id;
+  let oldRedeemLogs = slidingRedeemWindow.redeems;
+  let newRedeemLogs = new Array<string>();
+  newRedeemLogs.push(Redeem.id);
+
+  // add the current feeDetailLogEntry to the cumulative values
+  slidingRedeemWindow.cumulativeAmount += Redeem.amount;
+  slidingRedeemWindow.count += BigInt.fromI32(1);
+
+  // sliding window calculation
+  for (let i = 0; i < oldRedeemLogs.length; i++) {
+    // for every feeDetailLogEntry in the rolling window, check if they are old enough to remove
+    // if so, then remove and also decrease their values from cumulative rolling window values
+    let oldRedeem = RedeemEntity.load(oldRedeemLogs[i]);
+    if (!oldRedeem) continue;
+    if (Redeem.timestamp.minus(oldRedeem.timestamp) > BigInt.fromI32(86400)) {
+      slidingRedeemWindow.cumulativeAmount = slidingRedeemWindow.cumulativeAmount.minus(
+        oldRedeem.amount
+      );
+      slidingRedeemWindow.count -= BigInt.fromI32(1);
+    } else {
+      newRedeemLogs.push(oldRedeem.id);
+    }
+  }
+  slidingRedeemWindow.redeems = newRedeemLogs;
+  slidingRedeemWindow.save();
+}
+
+export function handleRewardsAccrued(event: RewardsAccrued): void {
+  const RewardsAccrued = new RewardsAccruedEntity(event.transaction.hash.toHex());
+  RewardsAccrued.userAddress = event.params.user;
+  RewardsAccrued.accruedRewards = event.params.amount.div(BigInt.fromI64(1000000000000000000));
+  RewardsAccrued.timestamp = event.block.timestamp;
+  RewardsAccrued.save();
+}
+
+export function handleRewardsClaimed(event: RewardsClaimed): void {
+  const RewardsClaimed = new RewardsClaimedEntity(event.transaction.hash.toHex());
+  RewardsClaimed.userAddress = event.params.to;
+  RewardsClaimed.amountToClaim = event.params.amount.div(BigInt.fromI64(1000000000000000000));
+  RewardsClaimed.timestamp = event.block.timestamp;
+  RewardsClaimed.save();
+}
+
+export function handleStaked(event: Staked): void {
+  const Staked = new StakedEntity(event.transaction.hash.toHex());
+  Staked.userAddress = event.params.from;
+  Staked.amount = event.params.amount.div(BigInt.fromI64(1000000000000000000));
+  Staked.timestamp = event.block.timestamp;
+
+  let userStakedTotal = UserStakedTotal.load("0");
+  if (!userStakedTotal){
+    userStakedTotal = new UserStakedTotal("0");
+    userStakedTotal.totalStaked = BigInt.fromI32(0);
+    userStakedTotal.timesStaked = BigInt.fromI32(0);
+  }
+  userStakedTotal.totalStaked += Staked.amount;
+  userStakedTotal.timesStaked += BigInt.fromI32(1);
+  userStakedTotal.save();
+
+  let uniqueWallet = UniqueWallet.load(event.params.from.toHex());
+  if (uniqueWallet == null) {
+    let uniqueWalletCount = UniqueWalletCount.load("0");
+
+    if (uniqueWalletCount == null) {
+      uniqueWalletCount = new UniqueWalletCount("0");
+      uniqueWalletCount.count = BigInt.fromI32(0);
+    }
+
+    uniqueWallet = new UniqueWallet(event.params.from.toHexString());
+    uniqueWallet.count = BigInt.fromI32(0);
+
+    uniqueWalletCount.count = uniqueWalletCount.count.plus(BigInt.fromI32(1));
+
+    uniqueWalletCount.save();
+  }
+
+  uniqueWallet.count = uniqueWallet.count.plus(BigInt.fromI32(1));
+  uniqueWallet.save();
+
+    let slidingStakedWindow = RollingStakedForLast24Hour.load("0");
+
+    if (!slidingStakedWindow) {
+      slidingStakedWindow = new RollingStakedForLast24Hour("0");
+      slidingStakedWindow.cumulativeAmount = BigInt.fromI32(0);
+      slidingStakedWindow.count = BigInt.fromI32(0);
+      slidingStakedWindow.stakes = new Array<string>();
+    }
+  
+    // add the current feeDetailLogEntry to the sliding window
+    // deposit.rollingWindow = slidingWindow.id;
+    let oldStakedLogs = slidingStakedWindow.stakes;
+    let newStakedLogs = new Array<string>();
+    newStakedLogs.push(Staked.id);
+  
+    // add the current feeDetailLogEntry to the cumulative values
+    slidingStakedWindow.cumulativeAmount += Staked.amount;
+    slidingStakedWindow.count += BigInt.fromI32(1);
+  
+    // sliding window calculation
+    for (let i = 0; i < oldStakedLogs.length; i++) {
+      // for every feeDetailLogEntry in the rolling window, check if they are old enough to remove
+      // if so, then remove and also decrease their values from cumulative rolling window values
+      let oldStake = StakedEntity.load(oldStakedLogs[i]);
+      if (!oldStake) continue;
+      if (Staked.timestamp.minus(oldStake.timestamp) > BigInt.fromI32(86400)) {
+        slidingStakedWindow.cumulativeAmount = slidingStakedWindow.cumulativeAmount.minus(
+          oldStake.amount
+        );
+        slidingStakedWindow.count -= BigInt.fromI32(1);
+      } else {
+        newStakedLogs.push(oldStake.id);
+      }
+    }
+    slidingStakedWindow.stakes = newStakedLogs;
+    slidingStakedWindow.save();
+    Staked.save();
+}
 
 export function handleTransfer(event: Transfer): void {}
 
